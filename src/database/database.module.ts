b2e2426070ee7b726eec1747df00aaa0779e2c97
gioocharkviani <gioocharkviani@gmail.com>
2026-06-27 21:52:47 +0400

    fixing

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,17 +1,22 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3333,
-      username: "root",
-      password: "gioocharkviani",
-      database: "mtiskari",
-      entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "mysql",
+        host: config.get<string>("DB_HOST"),
+        port: config.get<number>("DB_PORT"),
+        username: config.get<string>("DB_USERNAME"),
+        password: config.get<string>("DB_PASSWORD"),
+        database: config.get<string>("DB_DATABASE"),
+        entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+        synchronize: false,
+      }),
     }),
   ],
   exports: [TypeOrmModule],
